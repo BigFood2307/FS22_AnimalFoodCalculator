18ede8720687ad1a997aa72aaf9881e3971ed3d2
Lukas Pindl <lukas.pindl@gmx.net>
2023-08-17 11:12:41 +0200

    readme, usability

---
+++ README.md
@@ -0,0 +1,27 @@
+# Animal Food Calculator
+
+This script will calculate your food needs over a given amount of time.
+
+All config is done in the first few lines of main.py:
+
+* Choose your base animals xml (animals is vanilla, eas_animals with EAS, copy in from a map if different)
+* Choose your override xml (M+, M+ and EAS, custom). Use None if you dont use M+
+* Give the path to your savegames placeables.xml. All animals will be read from there
+* List all farmIds for which the animals should be read, other farms will be ignored
+* Give the timeframe in months for which the calculation should be done
+* In the age filter set [minAge, maxAge] only animals included in this range will be used when calculating the food amount
+* List all the animal subtypes, that you want to calculate the in/outputs for
+* Set your daysPerMonth for the daily food calculation
+
+Then just run the script...
+
+The given amounts are for the entire timeframe, values in brackets behind it are average per day.
+
+## Python installation
+
+If you have no idea how to run this script but still trust me enough to run some code you dont understand, here is how:
+
+* Install Python 3: https://www.python.org/downloads/
+* Open this folder in a command line: https://www.wikihow.com/Open-a-Folder-in-Cmd
+* Type 'python main.py' (without quotes)
+* Profit
--- main.py
+++ main.py
@@ -6,13 +6,22 @@ from animal_organization import AnimalHubandry, AnimalCluster
 
 from utility import	findSubType
 
+# Config start
+
 baseXmlPath = "xmls/eas_animals.xml"
-overrideXmlPath = "xmls/afa_eas_animalDataOverride.xml"
+overrideXmlPath = "xmls/afa_eas_animalDataOverride.xml" # None if no M+ is used
 placeablesXml = "xmls/sample_placeables.xml"
 
-farmIds = [1]
+farmIds = [0, 1]
 
 timeframe = 12
+
+ageFilter = [8, 1000]
+subTypeFilter = ["COW_HOLSTEIN", "BULL_HOLSTEIN"]
+
+daysPerMonth = 4
+
+# Config end
 
 baseElement = ET.parse(baseXmlPath).getroot()
 
@@ -23,24 +32,15 @@ for animalElement in baseElement.iter('animal'):
 	for subTypeElement in animalElement.iter('subType'):
 		subtypes.append(AnimalSubType(type, subTypeElement))
 
-overrideElement = ET.parse(overrideXmlPath).getroot()
+if overrideXmlPath is not None:
+	overrideElement = ET.parse(overrideXmlPath).getroot()
 
-for subTypeElement in overrideElement.iter('animalData'):
-	type = subTypeElement.attrib['type']
-	subType = subTypeElement.attrib['subType']
-	idx = findSubType(subtypes, subType, type)
-	if -1 != idx:
-		subtypes[idx].override(subTypeElement)
-
-"""
-for st in subtypes:
-	if len(st.outputs) > 0:
-		print(st.type + "/" + st.subType + ": ")
-		for key in iter(st.outputs):
-			print("\t" + str(st.outputs[key].getValue(28)) + " " + key)
-	else:
-		print(st.type + "/" + st.subType)
-"""
+	for subTypeElement in overrideElement.iter('animalData'):
+		type = subTypeElement.attrib['type']
+		subType = subTypeElement.attrib['subType']
+		idx = findSubType(subtypes, subType, type)
+		if -1 != idx:
+			subtypes[idx].override(subTypeElement)
 
 husbandries = AnimalHubandry.allFromPlaceables(placeablesXml, farmIds, subtypes)
 
@@ -48,17 +48,21 @@ outputs = Counter()
 inputs = Counter()
 
 for month in range(timeframe):
-	newIn, newOut = AnimalHubandry.calcInOutAll(husbandries, month, [8, 1000], ["COW_HOLSTEIN"])
+	newIn, newOut = AnimalHubandry.calcInOutAll(husbandries, month, ageFilter, subTypeFilter)
 	outputs.update(newOut)
 	inputs.update(newIn)
 
 outputs = dict(outputs)
 inputs = dict(inputs)
 
-print("Next Years inputs:")
+print("Inputs:")
 for key in iter(inputs):
-	print(key + ": " + str(inputs[key]))
+	perDay = inputs[key]/(12*daysPerMonth)
+	print(key + ": " + str(inputs[key]) + " (" + str(perDay) + ")")
 
-print("Next Years outputs:")
+print()
+
+print("Outputs:")
 for key in iter(outputs):
-	print(key + ": " + str(outputs[key]))
+	perDay = outputs[key]/(12*daysPerMonth)
+	print(key + ": " + str(outputs[key]) + " (" + str(perDay) + ")")
